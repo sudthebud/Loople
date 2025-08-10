e7d8a36f796ef7ffe23ea2e446a900a1d0deeceb
sudthebud <sudharshan.srinivasan.1@gmail.com>
2025-08-10 00:40:33 -0400

    CSS for game elements set up

--- javascript/classes/WordSetup.js
+++ javascript/classes/WordSetup.js
@@ -11,6 +11,8 @@ class WordSetup {
     static wordLoop
 
 
+    //Function to read JSON files
+    //(NEEDS TO BE CONVERTED TO REMOVE DEPRECATED OBJECT)
     static #ReadFile(fileName) {
         var request = new XMLHttpRequest()
         request.open("GET", `..\\..\\${assetPath}\\${datasetPath}\\${fileName}.json`, false)
@@ -20,8 +22,8 @@ class WordSetup {
         return jsonResult
     }
 
+    //Function to obtain word database objects
     static GetWordDatabase() {
-        console.log(this.#ReadFile("threeLetter"))
         this.#wordDictThreeLetter = this.#ReadFile("threeLetter")
         this.#wordDictFourLetter = this.#ReadFile("fourLetter")
         this.#wordDictFiveLetter = this.#ReadFile("fiveLetter")
@@ -31,6 +33,7 @@ class WordSetup {
         return Object.keys(this.#wordDictThreeLetter).length
     }
 
+    //Function with algorithm to create word loop array
     static GetWordLoop() {
         var numWords = maxNumWords
 
@@ -48,11 +51,13 @@ class WordSetup {
             var randomWordDict = wordDictList[RandomInt(wordDictList.length)]
 
 
+            //Finds first word
             if (this.wordLoop.length == 0) {
                 var key = Object.keys(randomWordDict)[RandomInt(Object.keys(randomWordDict).length)]
                 var randomWord = randomWordDict[key][RandomInt(randomWordDict[key].length)]
             }
 
+            //Finds last word, which must start with last letter of previous word and end with first letter of beginning word
             else if (this.wordLoop.length == numWords - 1) {
                 var firstLetter = this.wordLoop[this.wordLoop.length - 1].charAt(this.wordLoop[this.wordLoop.length - 1].length -1)
                 var lastLetter = this.wordLoop[0].charAt(0)
@@ -67,6 +72,7 @@ class WordSetup {
                 }
             }
 
+            //Finds a word that starts with last letter of the previous word
             else {
                 var firstLetter = this.wordLoop[this.wordLoop.length - 1].charAt(this.wordLoop[this.wordLoop.length - 1].length - 1)
 
